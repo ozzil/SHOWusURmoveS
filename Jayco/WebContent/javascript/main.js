d61fb636f872da76b93978abe611ea2dc6ab986e
wangz34 <dev6840379ha@C7DEPC17100004.dev.g.softbank.co.jp>
2018-11-15 19:02:54 +0900

    サボったけど

--- Jayco/WebContent/javascript/main.js
+++ Jayco/WebContent/javascript/main.js
@@ -3,3 +3,32 @@
  *
  */
 document.write("hello world");
+
+var i = 21;
+
+switch(i){
+	case 20:
+	 break;
+	case 21:
+		alert("21");
+		break;
+	default:
+		alert("Other");
+}
+//Ifの代わりに
+var j = {'20':1,'21':2}[i]||0;
+alert(j);
+
+var num = 25
+
+switch (true) {
+	case num <= 0:
+		alert("Less than or equal0")
+		break;
+	case num > 0 && num <= 25:
+		alert("between 0 ~ 25");
+	    break
+	default:
+		alert("more than 25");
+		break;
+}
